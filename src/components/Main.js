189d61cf035b29c176fde0e816f9d81e56d4e351
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-06-18 14:41:12 -0500

    designing main page content

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,18 @@ import ProfilePic from "../images/profile-pic.jpg";
 export default function Main() {
   return (
     <section className="main-container" id="main">
+      <div className="intro">
+        <h1>Full Stack Developer</h1>
+        <span><p>certified in:</p></span>
+        <ul>
+          <li>MERN Stack</li>
+          <li>Front-End Development</li>
+          <li>Back-End Development</li>
+          <li>UI</li>
+        </ul>
+      </div>
+
       <img src={ProfilePic} alt="William Lowrimore" />
-      <h1>Main Content Container</h1>
     </section>
   );
 }
